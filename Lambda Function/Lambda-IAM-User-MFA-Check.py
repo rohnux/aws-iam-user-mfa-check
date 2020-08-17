e49055874792c6b2d94380c1dd17db0681e42840
rohan <rohan.gupta.ext@telekom-digital.com>
2020-08-17 21:50:24 +0530

    added IAM role & Lambda function for user MFA check

---
+++ Lambda Function/Lambda-IAM-User-MFA-Check.py
@@ -0,0 +1,26 @@
+import json
+
+#AWS SDK for Python
+import boto3 
+
+#client for IAM service
+client=boto3.client('iam') 
+
+def lambda_handler(event, context):
+    #retrieve all IAM users
+    response = client.list_users() 
+    #output from client.list_users()
+    print(response) 
+    
+    #loop through users to determine who has MFA
+    for user in response['Users']:
+        userMFA = client.list_mfa_devices(UserName=user['UserName'])
+        if len(userMFA['MFADevices']) == 0 :
+            print("This user doesn't have MFA activated: ", user['UserName'])
+        else:
+            print("This user has activated MFA: ", user['UserName'])
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Successful MFA check')
+    }
